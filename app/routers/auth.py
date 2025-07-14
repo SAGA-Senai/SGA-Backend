165effe32e9aa92b9b156b43e5d35250fbbfaebd
Carlos Dorth <carloseduardodorth123@gmail.com>
2025-07-14 14:07:12 -0300

    primeiro commit: estrutura/rotas login e resgistro feitas

---
+++ app/routers/auth.py
@@ -0,0 +1,60 @@
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import select
+from passlib.hash import bcrypt
+from app.core.database import SessionLocal
+from app.models.usuario import DimUsuario
+from app.schemas.auth import LoginRequest, LoginResponse
+from app.schemas.auth import RegisterRequest 
+
+router = APIRouter()
+
+async def get_db():
+    async with SessionLocal() as session:
+        yield session
+
+@router.post("/login", response_model=LoginResponse)
+async def login_user(data: LoginRequest, db: AsyncSession = Depends(get_db)):
+    query = select(DimUsuario).where(DimUsuario.email == data.email)
+    result = await db.execute(query)
+    user = result.scalar_one_or_none()
+
+    if not user or not bcrypt.verify(data.senha, user.senha):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Credenciais inválidas")
+
+    return LoginResponse(
+        idusuario=user.idusuario,
+        nome=user.nome,
+        email=user.email
+    )
+
+
+@router.post("/register", response_model=LoginResponse)
+async def register_user(data: RegisterRequest, db: AsyncSession = Depends(get_db)):
+    # 1. Verificar se o e-mail já existe
+    query = select(DimUsuario).where(DimUsuario.email == data.email)
+    result = await db.execute(query)
+    existing_user = result.scalar_one_or_none()
+
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email já registrado.")
+
+    # 2. Criar novo usuário com senha criptografada
+    hashed_password = bcrypt.hash(data.senha)
+    new_user = DimUsuario(
+        nome=data.nome,
+        email=data.email,
+        senha=hashed_password,
+        datanasc=data.datanasc,
+        dataentrada=data.dataentrada,
+    )
+
+    db.add(new_user)
+    await db.commit()
+    await db.refresh(new_user)
+
+    return LoginResponse(
+        idusuario=new_user.idusuario,
+        nome=new_user.nome,
+        email=new_user.email
+    )
